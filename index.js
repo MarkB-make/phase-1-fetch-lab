93aae9edbe6fa187e06d4a4dc53811dac41a29e4
Mark <jadebrian999@gmail.com>
2025-09-25 11:12:23 +0300

    changes done

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main'); // your <main> element
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    h2.textContent = book.name; // must be capital C
+    main.appendChild(h2);       // append to main
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', fetchBooks);
